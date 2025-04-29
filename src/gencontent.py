67591eb2f15bb3ce7d5d947d7412778377819172
Ookame <kontakt@kazecode.pl>
2025-04-29 11:30:29 +0200

    gh page

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ import os
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -19,6 +19,9 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
 
+    template = template.replace('href="/', f"href=\"{basepath}")
+    template = template.replace('src="/', f"src=\"{basepath}")
+
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
@@ -33,7 +36,7 @@ def extract_title(md):
             return line[2:]
     raise ValueError("no title found")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for root, _, files in os.walk(dir_path_content):
         for file in files:
             if file.endswith('.md'):
@@ -44,5 +47,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 generate_page(
                     md_path,
                     template_path,
-                    dest_path
+                    dest_path,
+                    basepath
                 )
--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
+import sys
 import shutil
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page, generate_pages_recursive
+from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+
+    basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+
+    print(f"Basepath is: {basepath}")
+
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -23,6 +29,7 @@ def main():
         os.path.join(dir_path_content),
         template_path,
         os.path.join(dir_path_public),
+        basepath
     )
 
 
